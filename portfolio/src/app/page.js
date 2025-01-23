19175fd82c6766fb34613566691e99b2f4f3818f
doomangang <sp0943@cau.ac.kr>
2025-01-24 02:56:35 +0900

    feature :page implementation

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,9 +1,34 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from 'react';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Intro from "@/components/Intro";
+import Skill from "@/components/Skill";
+import Site from "@/components/Site";
+import Port from "@/components/Port";
+import Contact from "@/components/Contact";
+import Skip from "@/components/Skip";
+import lenis from "@/utils/lenis";
+import link from "@/utils/link";
 
 export default function Home() {
+    useEffect(() => {
+        lenis();
+        link();
+    }, []);
+
   return (
-    <div>
-      next.js
-    </div>
+      <>
+          <Skip />
+          <Header />
+          <main id="main" role="main">
+              <Intro />
+              <Skill />
+              <Site />
+              <Port />
+              <Contact />
+          </main>
+          <Footer />
+      </>
   );
 }
