1f24ded7ff92571ef3307161d142cc82e4f3fd89
doomangang <sp0943@cau.ac.kr>
2025-01-26 18:38:32 +0900

    refact: constants mod, lenis effect deact-,

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,22 +6,22 @@ const Footer = () => {
     return (
         <footer id="footer" role="contentinfo">
             <div className="footer__inner">
-                <div className="footer__text">
-                    <span>webstoryboy</span>
-                    <span>© webs</span>
-                </div>
+                <h2 className="footer__text">
+                    <div>webstoryboy</div>
+                    <div>@webs</div>
+                </h2>
                 <div className="footer__info">
                     <div className="left">
                         <div className="title">
                             <a href="#">sign up</a>
                         </div>
-                        <p>회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
+                        <p className="desc">회원가입을 하시면 댓글과 게시판 기능을 이용할 수 있습니다.</p>
                     </div>
                     <div className="right">
                         <h3>social</h3>
                         <ul>
-                            {footerText.map((footer, index) => (
-                                <li key={index}>
+                            {footerText.map((footer, key) => (
+                                <li key={key}>
                                     <a href={footer.link}>{footer.title}</a>
                                     <em>{footer.desc}</em>
                                 </li>
@@ -30,8 +30,8 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer__right">
-                    © 2023 webstoryboy<br />
-                    이 사이트는 넥스트제이에스 이용하여 제작하였습니다.
+                    &copy; 2023 webstoryboy<br />
+                    이 사이트는 비트를 이용하여 제작하였습니다.
                 </div>
             </div>
         </footer>
